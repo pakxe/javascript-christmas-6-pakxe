e48e1034da351b8888ebd5c412bcd6dfef1fbfe0
pakxe <pigkill40@naver.com>
2023-11-12 21:06:12 +0900

    feat: 가격 계산을 담당하는 계산기 클래스

---
+++ src/model/PriceCalculator.js
@@ -0,0 +1,40 @@
+import MENU_LIST from '../constant/menuList.js';
+import menuListToFlatArr from '../utils/menuListToFlatArr.js';
+
+class PriceCalculator {
+  static getTotalPriceWithoutDiscount(menuList) {
+    const flattedMenuList = menuListToFlatArr(menuList);
+
+    const totalPrice = flattedMenuList.reduce(
+      (total, menu) => total + this.#calcMenuPrice(menu),
+      0,
+    );
+
+    return totalPrice;
+  }
+
+  static #calcMenuPrice(menu) {
+    const menuPrice = this.#getMenuPrice(menu.name);
+
+    return menuPrice * menu.count;
+  }
+
+  static #getMenuPrice(menuName) {
+    const { price } = MENU_LIST.find((menu) => menu.name === menuName);
+
+    return price;
+  }
+
+  static getTotalDiscountPrice(discountList) {
+    return discountList.reduce(
+      (total, discountInfo) => total + discountInfo.totalDiscountPrice,
+      0,
+    );
+  }
+
+  static getFinalPrice(totalPriceWithoutDiscount, totalDiscountPrice) {
+    return totalPriceWithoutDiscount - totalDiscountPrice;
+  }
+}
+
+export default PriceCalculator;
