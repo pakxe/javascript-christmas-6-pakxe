48874b7d525e1d100e82baaa93d29c7111f7908b
pakxe <pigkill40@naver.com>
2023-11-12 20:01:44 +0900

    feat: 이벤트 결과를 보여주는 메세지 작성

---
+++ src/view/constants/previewMessage.js
@@ -0,0 +1,29 @@
+import EVENT from '../../constant/eventInfo.js';
+
+const PREVIEW_MARK = {
+  open: '<',
+  close: '>',
+  count: '개',
+  price: '원',
+  none: '없음\n',
+  namePriceDivider: ':',
+  minus: '-',
+  newLine: '\n',
+};
+
+// 일괄 수정을 위한 래핑 스트링
+const wrapPreview = (message) =>
+  `${PREVIEW_MARK.open}${message}${PREVIEW_MARK.close}`;
+
+// TODO: 반복되고, 함수명이 상수형인거 수정해야함.
+const PREVIEW_HEADER = {
+  menuList: wrapPreview('주문 메뉴'),
+  totalPriceWithoutDiscount: wrapPreview('할인 전 총주문 금액'),
+  giftList: wrapPreview('증정 메뉴'),
+  discountList: wrapPreview('혜택 내역'),
+  totalDiscountPrice: wrapPreview('총혜택 금액'),
+  finalPrice: wrapPreview('할인 후 예상 결제 금액'),
+  badge: wrapPreview(`${EVENT.month}월 이벤트 배지`),
+};
+
+export { PREVIEW_HEADER, PREVIEW_MARK };
