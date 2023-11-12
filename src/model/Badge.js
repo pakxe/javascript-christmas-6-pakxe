3e5f0a181e086fa229ad6f91be1c375a48ea1b15
pakxe <pigkill40@naver.com>
2023-11-12 21:04:09 +0900

    feat: 배지 증정을 위한 클래스

---
+++ src/model/Badge.js
@@ -0,0 +1,28 @@
+const BADGE_LIST = [
+  {
+    name: '별',
+    minPrice: 5_000,
+  },
+  {
+    name: '트리',
+    minPrice: 10_000,
+  },
+  {
+    name: '산타',
+    minPrice: 20_000,
+  },
+];
+
+class Badge {
+  static checkBadge(totalDiscountPrice) {
+    let badge = null;
+
+    BADGE_LIST.forEach(({ name, minPrice }) => {
+      if (totalDiscountPrice >= minPrice) badge = name;
+    });
+
+    return badge;
+  }
+}
+
+export default Badge;
