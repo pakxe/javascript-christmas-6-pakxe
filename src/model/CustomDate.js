f588bc2bf4639f6f510576bc3c0bdea77ee58a2c
pakxe <pigkill40@naver.com>
2023-11-12 17:41:34 +0900

    feat: 방문일과 이벤트 기간을 다루기 위한 날짜 클래스

---
+++ src/model/CustomDate.js
@@ -0,0 +1,42 @@
+import ERROR from '../error/constants/error.js';
+import EVENT from '../constant/eventInfo.js';
+import VisitDayError from '../error/VisitDayError.js';
+import Parser from '../parser/Parser.js';
+import Validator from '../validator/Validator.js';
+
+class CustomDate {
+  #date;
+
+  constructor(day) {
+    const parsedDay = Parser.parseInt(day);
+
+    this.#validateDay(parsedDay);
+
+    // 확장 가능성을 생각해 Date 객체로 다룸
+    this.#date = new Date(
+      `${EVENT.year}-${EVENT.month}-${Parser.addLeadingZero(day, 2)}`,
+    );
+  }
+
+  #validateDay(day) {
+    if (Validator.isNan(day)) throw new VisitDayError(ERROR.visitDay);
+    if (Validator.isNotInRange([EVENT.startDay, EVENT.endDay], day))
+      throw new VisitDayError(ERROR.visitDay);
+  }
+
+  get() {
+    return this.#date;
+  }
+
+  // 년월일 중 일
+  get date() {
+    return this.#date.getDate();
+  }
+
+  // 요일
+  get day() {
+    return this.#date.getDay();
+  }
+}
+
+export default CustomDate;
