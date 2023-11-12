e9f2a162126faeaea7951b545da51f27f652760d
pakxe <pigkill40@naver.com>
2023-11-12 21:02:06 +0900

    feat: 증정 이벤트를 위한 이벤트 객체 작성

---
+++ src/model/event/GiftEvent.js
@@ -0,0 +1,54 @@
+import Event from './Event.js';
+import findMenu from '../../utils/findMenu.js';
+
+const GIFT_LIST = [{ giftName: '샴페인', minPrice: 120_000 }];
+
+// 24만원엔 다른 상품을 주는 식의 확장이 있을 수 있다. 따라서 맵으로 관리
+class GiftEvent extends Event {
+  #giftList = new Map(); // name: count
+
+  #totalDiscountPrice;
+
+  init({ shoppingCart }) {
+    this.#giftList = this.#calcGiftList(shoppingCart.totalPrice);
+    this.#totalDiscountPrice = this.#calcTotalDiscountPrice();
+  }
+
+  #calcGiftList(totalPrice) {
+    GIFT_LIST.forEach((gift) => this.#giveGiftIfOverPrice(gift, totalPrice));
+
+    return this.#giftList;
+  }
+
+  #giveGiftIfOverPrice({ giftName, minPrice }, totalPrice) {
+    if (totalPrice < minPrice) return;
+
+    if (this.#giftList.has(giftName))
+      this.#giftList.set(giftName, this.#giftList.get(giftName) + 1);
+    else this.#giftList.set(giftName, 1);
+  }
+
+  #calcTotalDiscountPrice() {
+    let totalPrice = 0;
+
+    this.#giftList.forEach((count, name) => {
+      totalPrice += findMenu(name).price * count;
+    });
+
+    return totalPrice;
+  }
+
+  get giftList() {
+    const giftListArr = [];
+
+    this.#giftList.forEach((count, name) => giftListArr.push({ name, count }));
+
+    return giftListArr;
+  }
+
+  get totalDiscountPrice() {
+    return this.#totalDiscountPrice;
+  }
+}
+
+export default GiftEvent;
