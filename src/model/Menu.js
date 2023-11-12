b54b26b2fd36e3a7689a67e76b71714bb2b72f8c
pakxe <pigkill40@naver.com>
2023-11-13 02:18:54 +0900

    refactor: 유효성 검증 로직 분리

--- src/model/Menu.js
+++ src/model/Menu.js
@@ -1,16 +1,12 @@
-import MENU_LIST from '../constant/menuList.js';
-import ERROR from '../error/constants/error.js';
 import Parser from '../parser/Parser.js';
-import Validator from '../validator/Validator.js';
-import { MENU_COUNT } from './constants/rule.js';
-import MenuError from '../error/MenuError.js';
+import validateMenu from '../validator/validateMenu.js';
 
 class Menu {
   #menu;
 
   constructor(menu) {
     const parsedMenu = this.#parseMenu(menu);
-    this.#validateMenu(parsedMenu);
+    validateMenu(parsedMenu);
 
     this.#menu = parsedMenu;
   }
@@ -18,22 +14,6 @@ class Menu {
   #parseMenu(menu) {
     const [name, count] = Parser.splitMenuInfo(menu);
     return { name, count: Parser.parseInt(count) };
-  }
-
-  #validateMenu({ name, count }) {
-    if (this.#isInvalidName(name)) throw new MenuError(ERROR.menu);
-    if (this.#isInvalidCount(count)) throw new MenuError(ERROR.menu);
-  }
-
-  #isInvalidName(menuName) {
-    const validMenuNameList = MENU_LIST.map(({ name }) => name);
-
-    return !validMenuNameList.includes(menuName);
-  }
-
-  // 합치면 오버틀로우일 수 있으므로 여기서부터 합을 계산한다.
-  #isInvalidCount(count) {
-    return Validator.isNotInRange([MENU_COUNT.min, MENU_COUNT.max], count);
   }
 
   get name() {
