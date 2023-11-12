2c7cddbd5ca3df4219df5341ac096c39ff826f24
pakxe <pigkill40@naver.com>
2023-11-12 19:45:25 +0900

    feat: 메뉴 개수 제한을 위한 값 상수화

---
+++ src/model/constants/menu.js
@@ -0,0 +1,13 @@
+const MENU_COUNT = {
+  min: 1,
+  max: 20,
+};
+
+const MENU_LIST_COUNT = {
+  min: 1,
+  max: 20,
+};
+
+const MIN_ORDER_PRICE = 10_000;
+
+export { MENU_COUNT, MENU_LIST_COUNT, MIN_ORDER_PRICE };
