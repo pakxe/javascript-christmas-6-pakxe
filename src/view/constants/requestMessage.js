a99d276a7a512188e5627cee7935647cf62edc2c
pakxe <pigkill40@naver.com>
2023-11-13 15:07:51 +0900

    rename: 상수명 수정으로 인한 변경

--- src/view/constants/requestMessage.js
+++ src/view/constants/requestMessage.js
@@ -1,5 +1,5 @@
 import DIVIDER from '../../constant/divider.js';
-import EVENT_LIST from '../../model/constants/eventList.js';
+import EVENT from '../../constant/event.js';
 import deepFreeze from '../../utils/deepFreeze.js';
 
 const INPUT_HINT = {
@@ -9,7 +9,7 @@ const INPUT_HINT = {
 
 const REQUEST_MESSAGE = deepFreeze({
   ...INPUT_HINT,
-  visitDate: `${EVENT_LIST.month}월 중 식당 예상 방문 날짜는 언제인가요? ${INPUT_HINT.number}\n`,
+  visitDate: `${EVENT.month}월 중 식당 예상 방문 날짜는 언제인가요? ${INPUT_HINT.number}\n`,
   menuList: `주문하실 메뉴를 메뉴와 개수를 알려주세요. ${INPUT_HINT.menu}\n`,
 });
 
