8db9cae8acee0b3896d0563180573a3eda0ecfe4
pakxe <pigkill40@naver.com>
2023-11-15 03:58:12 +0900

    test: 쇼핑 카트가 총 가격과 카테고리별메뉴를 잘 반환하는지 확인하는 테스트 추가

--- __tests__/model/ShoppingCartTest.js
+++ __tests__/model/ShoppingCartTest.js
@@ -1,10 +1,13 @@
+import { CATEGORY } from '../../src/constant/menuList.js';
 import { ERROR } from '../../src/error/constants/error.js';
 import ShoppingCart from '../../src/model/ShoppingCart.js';
 
 describe('장바구니 테스트', () => {
+  const menuListInput = '아이스크림-1,티본스테이크-2,제로콜라-3,시저샐러드-4';
+  const shoppingCart = new ShoppingCart(menuListInput);
+
+  // 정상인 경우에 대한 테스트
   test('올바른 메뉴 목록 입력일 경우 바르게 생성되는지 확인', () => {
-    const menuListInput = '아이스크림-1,티본스테이크-2,제로콜라-3,시저샐러드-4';
-    const shoppingCart = new ShoppingCart(menuListInput);
     const menuList = shoppingCart.menuList.map(({ name, count }) => ({
       name,
       count,
@@ -16,6 +19,19 @@ describe('장바구니 테스트', () => {
       { name: '제로콜라', count: 3 },
       { name: '시저샐러드', count: 4 },
     ]);
+  });
+
+  test('올바르게 총 가격을 반환하는지 확인', () => {
+    // 5000 + 55000 * 2 + 3000 * 3 + 8000 * 4
+    expect(shoppingCart.totalPrice).toEqual(156000);
+  });
+
+  test('카테고리에 해당하는 메뉴들을 잘 반환하는지 확인', () => {
+    const menuList = shoppingCart
+      .getListOfCategory(CATEGORY.beverage)
+      .map(({ name, count }) => ({ name, count }));
+
+    expect(menuList).toEqual([{ name: '제로콜라', count: 3 }]);
   });
 
   // 오류가 나는 경우에 대한 테스트
