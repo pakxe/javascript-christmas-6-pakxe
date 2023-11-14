af0201c49a3086e142b6c605c41bad30234fa4e7
pakxe <pigkill40@naver.com>
2023-11-14 15:57:29 +0900

    feat: 주문번호 입력에 대한 오류 메세지 추가

--- src/error/constants/error.js
+++ src/error/constants/error.js
@@ -3,6 +3,7 @@ import deepFreeze from '../../utils/deepFreeze.js';
 
 const COMMON_MESSAGE = {
   retry: '다시 입력해 주세요.',
+  limitRetry: (count) => `주문번호 입력 ${count}회 오류. `,
 };
 
 const ERROR = deepFreeze({
@@ -12,6 +13,8 @@ const ERROR = deepFreeze({
   menu: `유효하지 않은 주문입니다. ${COMMON_MESSAGE.retry}`,
   onlyBeverage: `음료만 포함되어 있는 경우 주문할 수 없습니다. ${COMMON_MESSAGE.retry}`,
   underMinOrderPrice: `총 주문 금액이 ${MIN_ORDER_PRICE}원 이상이어야 주문할 수 있습니다. ${COMMON_MESSAGE.retry}`,
+  notFoundOrderId: '입력하신 주문 번호에 해당하는 정보가 존재하지 않습니다. ',
+  exit: '프로그램을 종료합니다.',
 });
 
-export default ERROR;
+export { ERROR, COMMON_MESSAGE };
