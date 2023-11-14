9d35b68dc3c42feee2f7d45b5bdbc14e414aa30b
pakxe <pigkill40@naver.com>
2023-11-14 19:48:27 +0900

    comment: 해당 자료구조를 사용한 이유를 이벤트 목표와 함께 작성

--- src/model/event/ChampagneEvent.js
+++ src/model/event/ChampagneEvent.js
@@ -3,6 +3,10 @@ import findMenu from '../../utils/findMenu.js';
 import GiftEvent from './GiftEvent.js';
 
 // 24만원엔 다른 상품을 주는 식의 확장이 있을 수 있다. 따라서 맵으로 관리
+/**
+ * 중복된 할인과 증정을 허용해서, 고객들이 혜택을 많이 받는다는 것을 체감할 수 있게 하는 것
+ * 위 목표를 달성하기 위해 증정 항목이 늘어나도 기존 기준에서 받은 증정품도 유지되도록 하였습니다.
+ */
 const GIFT_LIST = [{ giftName: '샴페인', minPrice: 120_000 }];
 
 class ChampagneEvent extends GiftEvent {
