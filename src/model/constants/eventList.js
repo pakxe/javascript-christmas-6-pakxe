1d9f348e145cab845e17936e58eaf240b9500a8f
pakxe <pigkill40@naver.com>
2023-11-14 16:00:29 +0900

    feat: 새해 이벤트 추가

--- src/model/constants/eventList.js
+++ src/model/constants/eventList.js
@@ -1,45 +1,51 @@
-import DateUtils from '../../utils/DateUtils.js';
 import deepFreeze from '../../utils/deepFreeze.js';
-import EVENT from '../../constant/event.js';
+import generateEventPeriod from '../../utils/generateEventPeriod.js';
 
-const MONTHLY_EVENT_PERIOD = deepFreeze({
-  start: new Date(`${EVENT.year}-${EVENT.month}-01${EVENT.startTime}`),
-  end: new Date(
-    `${EVENT.year}-${EVENT.month}-${DateUtils.getLastDayOfMonth(
-      EVENT.year,
-      EVENT.month,
-    )}${EVENT.endTime}`,
-  ),
-});
+const UNTIL_CHRISTMAS = {
+  start: '2023.12.01',
+  end: '2023.12.25',
+};
 
-const EVENT_LIST = Object.freeze({
+export const MONTHLY = {
+  start: '2023.12.01',
+  end: '2023.12.31',
+};
+
+export const NEW_YEAR = {
+  start: '2024.01.01',
+  end: '2024.01.31',
+};
+
+const EVENT_LIST = deepFreeze({
   christmasDDay: {
     name: '크리스마스 디데이 이벤트',
     engName: 'christmasDDay',
-    period: {
-      start: new Date(`${EVENT.year}-${EVENT.month}-01${EVENT.startTime}`),
-      end: new Date(`${EVENT.year}-${EVENT.month}-25${EVENT.endTime}`),
-    },
+    period: generateEventPeriod(UNTIL_CHRISTMAS),
   },
   weekdays: {
     name: '평일 할인',
     engName: 'weekdays',
-    period: MONTHLY_EVENT_PERIOD,
+    period: generateEventPeriod(MONTHLY),
   },
   weekend: {
     name: '주말 할인',
     engName: 'weekend',
-    period: MONTHLY_EVENT_PERIOD,
+    period: generateEventPeriod(MONTHLY),
   },
   star: {
     name: '특별 할인',
     engName: 'star',
-    period: MONTHLY_EVENT_PERIOD,
+    period: generateEventPeriod(MONTHLY),
   },
   gift: {
     name: '증정 이벤트',
     engName: 'gift',
-    period: MONTHLY_EVENT_PERIOD,
+    period: generateEventPeriod(MONTHLY),
+  },
+  newYear: {
+    name: '새해 이벤트',
+    engName: 'newYear',
+    period: generateEventPeriod(NEW_YEAR),
   },
 });
 
