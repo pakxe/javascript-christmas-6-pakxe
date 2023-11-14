68b818118ae3dcf4cf67d305e8feb54ca58cf6de
pakxe <pigkill40@naver.com>
2023-11-14 16:44:57 +0900

    feat: 메인 미션과 서브 미션 연결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import EventController from './controller/EventController.js';
+import NewYearController from './controller/NewYearEventController.js';
+import OrderHistory from './model/OrderHistory.js';
+
 class App {
-  async run() {}
+  async run() {
+    const orderHistory = new OrderHistory();
+
+    await this.#eventPlanner(orderHistory);
+    // await this.#checkNewYearGift(orderHistory);
+  }
+
+  async #eventPlanner(orderHistory) {
+    const controller = new EventController(orderHistory);
+
+    await controller.process(orderHistory);
+  }
+
+  async #checkNewYearGift(orderHistory) {
+    const newYearController = new NewYearController();
+
+    await newYearController.process(orderHistory);
+  }
 }
 
 export default App;
