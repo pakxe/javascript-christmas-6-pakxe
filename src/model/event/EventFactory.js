f9b6d1db537e7ef83f997725b397e68896e15053
pakxe <pigkill40@naver.com>
2023-11-14 14:38:35 +0900

    refactor: 이벤트 반환하는 부분 가독성을 위해 분리

--- src/model/event/EventFactory.js
+++ src/model/event/EventFactory.js
@@ -1,10 +1,11 @@
 import EVENT_LIST from '../constants/eventList.js';
 import ChristmasDDayEvent from './ChristmasDDayEvent.js';
-import GiftEvent from './GiftEvent.js';
 import StarEvent from './StarEvent.js';
 import WeekDaysEvent from './WeekDaysEvent.js';
 import WeekendEvent from './WeekendEvent.js';
 import FactoryError from '../../error/FactoryError.js';
+import NewYearEvent from './NewYearEvent.js';
+import ChampagneEvent from './ChampagneEvent.js';
 
 class EventFactory {
   static create(name) {
@@ -12,6 +13,10 @@ class EventFactory {
     if (!Object.prototype.hasOwnProperty.call(EVENT_LIST, name))
       throw new FactoryError();
 
+    return this.#checkMainEvent(name);
+  }
+
+  static #checkMainEvent(name) {
     if (name === EVENT_LIST.christmasDDay.engName)
       return new ChristmasDDayEvent(EVENT_LIST.christmasDDay);
     if (name === EVENT_LIST.weekdays.engName)
@@ -19,7 +24,16 @@ class EventFactory {
     if (name === EVENT_LIST.weekend.engName)
       return new WeekendEvent(EVENT_LIST.weekend);
     if (name === EVENT_LIST.star.engName) return new StarEvent(EVENT_LIST.star);
-    if (name === EVENT_LIST.gift.engName) return new GiftEvent(EVENT_LIST.gift);
+    if (name === EVENT_LIST.gift.engName)
+      return new ChampagneEvent(EVENT_LIST.gift);
+
+    return this.#checkNewYearEvent(name); // 체인 형식으로 이벤트 호출.. 줄이 길어지는 것을 막기위해 분리하다보니 불가피하게 이런 방식을 택하게 되었음
+  }
+
+  static #checkNewYearEvent(name) {
+    // 2024.1월을 위한 새해 이벤트
+    if (name === EVENT_LIST.newYear.engName)
+      return new NewYearEvent(EVENT_LIST.newYear);
   }
 }
 
