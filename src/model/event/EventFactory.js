d1c3587582a93871d53184f759299347bb58b6d2
pakxe <pigkill40@naver.com>
2023-11-14 16:45:35 +0900

    refactor: 같은 코드를 줄이기 위한 배열화

--- src/model/event/EventFactory.js
+++ src/model/event/EventFactory.js
@@ -8,32 +8,25 @@ import NewYearEvent from './NewYearEvent.js';
 import ChampagneEvent from './ChampagneEvent.js';
 
 class EventFactory {
+  static #eventMap = new Map([
+    [EVENT_LIST.christmasDDay.engName, ChristmasDDayEvent],
+    [EVENT_LIST.weekdays.engName, WeekDaysEvent],
+    [EVENT_LIST.weekend.engName, WeekendEvent],
+    [EVENT_LIST.star.engName, StarEvent],
+    [EVENT_LIST.gift.engName, ChampagneEvent],
+    [EVENT_LIST.newYear.engName, NewYearEvent],
+  ]);
+
   static create(name) {
     // 해당하는 이벤트가 없으면 팩토리 오류
     if (!Object.prototype.hasOwnProperty.call(EVENT_LIST, name))
       throw new FactoryError();
 
-    return this.#findEvent(name);
-  }
+    const EventClass = this.#eventMap.get(name);
 
-  static #findEvent(name) {
-    if (name === EVENT_LIST.christmasDDay.engName)
-      return new ChristmasDDayEvent(EVENT_LIST.christmasDDay);
-    if (name === EVENT_LIST.weekdays.engName)
-      return new WeekDaysEvent(EVENT_LIST.weekdays);
-    if (name === EVENT_LIST.weekend.engName)
-      return new WeekendEvent(EVENT_LIST.weekend);
-    if (name === EVENT_LIST.star.engName) return new StarEvent(EVENT_LIST.star);
-    if (name === EVENT_LIST.gift.engName)
-      return new ChampagneEvent(EVENT_LIST.gift);
-
-    return this.#findNewYearEvent(name); // 체인 형식으로 이벤트 호출.. 줄이 길어지는 것을 막기위해 분리하다보니 불가피하게 이런 방식을 택하게 되었음
-  }
-
-  static #findNewYearEvent(name) {
-    // 2024.1월을 위한 새해 이벤트
-    if (name === EVENT_LIST.newYear.engName)
-      return new NewYearEvent(EVENT_LIST.newYear);
+    if (EventClass) {
+      return new EventClass(EVENT_LIST[name]);
+    }
   }
 }
 
